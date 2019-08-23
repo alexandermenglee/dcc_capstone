58f70ba4dd232d49bbffffd76574c32a5162de53
Alexander Lee <alexandermlee10@gmail.com>
2019-08-23 07:54:32 -0500

    added radio buttons so a user can select their gender when entering in their statistics

--- Fitbook/Data/Migrations/20190823123101_added gender property to fitbookuser.Designer.cs
+++ Fitbook/Data/Migrations/20190823123101_added gender property to fitbookuser.Designer.cs
@@ -4,14 +4,16 @@ using Fitbook.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fitbook.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190823123101_added gender property to fitbookuser")]
+    partial class addedgenderpropertytofitbookuser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
